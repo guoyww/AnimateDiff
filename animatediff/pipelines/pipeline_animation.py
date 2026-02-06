a67187a24986b5b9db7b03f513b70eb8a6c6be7d
Mr-Neutr0n <64578610+Mr-Neutr0n@users.noreply.github.com>
2026-02-07 05:19:48 +0530

    Fix diffusers compatibility: handle renamed pipeline_utils module

--- animatediff/pipelines/pipeline_animation.py
+++ animatediff/pipelines/pipeline_animation.py
@@ -14,7 +14,10 @@ from transformers import CLIPTextModel, CLIPTokenizer
 
 from diffusers.configuration_utils import FrozenDict
 from diffusers.models import AutoencoderKL
-from diffusers.pipeline_utils import DiffusionPipeline
+try:
+    from diffusers.pipeline_utils import DiffusionPipeline
+except ImportError:
+    from diffusers.pipelines.pipeline_utils import DiffusionPipeline
 from diffusers.schedulers import (
     DDIMScheduler,
     DPMSolverMultistepScheduler,
